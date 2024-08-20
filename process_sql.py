0f5689bcdfe04aa9b279fd4d46b5a98b3c41ee64
felix01189 <felix01189@naver.com>
2024-08-20 14:08:45 +0900

    for order table in financial db:

--- process_sql.py
+++ process_sql.py
@@ -94,6 +94,7 @@ def get_schema(db):
 
     # fetch table info
     for table in tables:
+        table = f'"{table}"'
         cursor.execute("PRAGMA table_info({})".format(table))
         schema[table] = [str(col[1].lower()) for col in cursor.fetchall()]
 
